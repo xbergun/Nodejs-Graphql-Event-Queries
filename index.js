1fa2f8a1603df78568e0ae7c8e7434ece888e2de
Berkayergun16 <berkay.ergun16@gmail.com>
2022-03-01 16:47:05 +0300

    works

--- index.js
+++ index.js
@@ -37,7 +37,96 @@ const {deleteUser,
 
 const { typeDefsQuery } = require("./typeDefs");
 
-const typeDefs = typeDefsQuery;
+const typeDefs = gql`
+type User {
+  id: ID!
+  username: String!
+  email: String!
+  events: [events]!
+  
+}
+type events {
+  id: ID!
+  title: String!
+  desc: String!
+  date: String!
+  from: String!
+  to: String!
+  location_id: String!
+  user_id: String!
+  location: locations
+  participants: [participants]!
+  user: User
+}
+type locations {
+  id: ID!
+  name: String!
+  desc: String!
+  lat: String!
+  lng: String!
+}
+
+type participants {
+  id: ID!
+  user_id: String!
+  event_id: String!
+}
+
+type Query {
+   # users
+   users: [User!]!
+   user (id: ID!): User!
+
+  # events
+  events: [events!]!
+  event(id: ID!): events!
+
+  # locations
+  locations: [locations!]!
+  location(id: ID!): locations
+
+  # participants
+  participants: [participants!]!
+  participant(id: ID!): participants
+ 
+}
+type Mutation {
+  # users
+  createUser(username: String!, email: String!): User!
+
+  updateUser(id: ID!, username: String!, email: String!, password: String!): User!
+  
+  deleteUser(id: ID!): User!
+  deleteAllUsers: [User!]!
+  
+  # events
+  createEvent(title: String!, desc: String!, date: String!, from: String!, to: String!, location_id: String!, user_id: String!): events!
+
+  updateEvent(id: ID!, title: String!, desc: String!, date: String!, from: String!, to: String!, location_id: String!, user_id: String!): events!
+
+  deleteEvent(id: ID!): events!
+  deleteAllEvents: [events!]!
+
+  # locations
+  createLocation(name: String!, desc: String!, lat: String!, lng: String!): locations!
+
+  updateLocation(id: ID!, name: String!, desc: String!, lat: String!, lng: String!): locations!
+
+  deleteLocation(id: ID!): locations!
+  deleteAllLocations: [locations!]!
+
+  # participants
+  createParticipant(user_id: String!, event_id: String!): participants!
+
+  updateParticipant(id: ID!, user_id: String!, event_id: String!): participants!
+
+  deleteParticipant(id: ID!): participants!
+  deleteAllParticipants: [participants!]!
+
+
+
+}
+`;
 
 const resolvers = {
   Mutation: {
@@ -59,27 +148,64 @@ const resolvers = {
   Query: {
     //! User Queries
     users: () => users,
-    user: userQuery,
+    user: (parent, args) => {
+      const user = users.find(user => user.id === args.id);
+      if(!user){
+          throw Error("User not found")
+      }
+      return user;
+  },
     
 
     //! Events Queries
     events: () => events,
-    event: eventQuery,
+    event: (parent, args, context, info) => {
+      const event = events.find(event => event.id === args.id);
+      if(!event){
+          throw Error("Event not found")
+      }
+      return event;
+  },
 
     //! Locations Queries
     locations: () => locations,
-    location: locationQuery,
+    location: (parent, args, context, info) => {
+      const location = locations.find(location => location.id === args.id);
+      if(!location){
+          throw Error("Location not found")
+      }
+      return location},
 
     //! Participants Queries
     participants: () => participants,
-    participant: participantQuery,
+    participant:  (parent, args, context, info) => {
+      const participant = participants.find(participant => participant.id === args.id);
+              if(!participant){
+                  throw Error("Participant not found")
+              }
+              return participant;
+  }
+  
   },
   User: {
-    events: userEventQuery,
+    events: (parent) => {
+      return events.filter(event => event.user_id === parent.id);
+  
+  },
   },
   events: {
-    location: eventLocationQuery,
-    participants: eventParticipantsQuery,
+    location: (parent) => {
+      return locations.find(location => location.id === parent.location_id);
+  
+  },
+    participants: (parent) => {
+      return participants.filter(
+          participant => participant.event_id === parent.id
+        );
+  },
+    user:(parent) => {
+      return users.find(user => user.id === parent.user_id);
+    }
   },
 };
 
